043cc9a43308c07747c6359054bb8e00054447c6
Laziu Kim <laziu.cc@gmail.com>
2019-05-01 14:46:07 +0900

    Game 헤더 순서만 살짝 수정

--- 2019H1KimAI-Team24/2019H1KimAI-Team24/game.h
+++ 2019H1KimAI-Team24/2019H1KimAI-Team24/game.h
@@ -6,20 +6,21 @@
 class Game {
 public:
 	static constexpr byte FIRST = 0, SECOND = 1;
+	byte currentPlayer;
 private:
 	long long board = 0;
 	long long player[2] = { 0 };
 	byte count[7] = { 0 };
-public:
-	byte currentPlayer;
 
+public:
 	Game(byte first) : currentPlayer(first) {}
 	bool putStone(int column);
 	void printBoard();
+	bool endGame(); //°¡¸Á¾øÀ½
+
 	bool pos(byte pl, int row, int col) const {
 		return (player[pl] >> ((col * 7) + row)) & 1;
 	}
-	bool endGame(); //°¡¸Á¾øÀ½
 };
 
 #endif //GAME_H

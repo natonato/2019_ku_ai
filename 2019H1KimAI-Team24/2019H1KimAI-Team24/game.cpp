63990ed779a596f8149250892abdcc9a094ccb62
Laziu Kim <laziu.cc@gmail.com>
2019-05-01 15:35:01 +0900

    클린코드쓰자

--- 2019H1KimAI-Team24/2019H1KimAI-Team24/game.cpp
+++ 2019H1KimAI-Team24/2019H1KimAI-Team24/game.cpp
@@ -17,10 +17,10 @@ void Game::debugPrint() {
 }
 
 bool Game::putStone(int col) {
-	if (board & (1LL << ((col * 7) + 5))) {
+	if (board & pos(col, 5)) {
 		return false; // 둘 수 없을때
 	}
-	board = board | (board + (1LL << (col * 7)));
+	board = board | (board + pos(col, 0));
 	player[currentPlayer] = board ^ player[!currentPlayer];
 	currentPlayer = !currentPlayer;
 	return true;
@@ -30,19 +30,11 @@ void Game::printBoard() {
 	printf("Player1 : O Player2 : X\n");
 	for (int i = 5; i >= 0; i--) {
 		printf("\n  ├─┼─┼─┼─┼─┼─┼─┤\n");
-		long long bit = 1LL << i;
 		printf("%d │", i + 1);
 		for (int j = 0; j < 7; j++) {
-			char t = ' ';
-			if (player[0] & bit) {
-				t = 'O';
-			}
-			else if (player[1] & bit) {
-				t = 'X';
-			}
-			printf("%c", t);
-			printf("│");
-			bit <<= 7;
+			auto bit = pos(j, i);
+			char t = (player[FIRST] & bit) ? 'O' : (player[SECOND] & bit) ? 'X' : ' ';
+			printf("%c│", t);
 		}
 	}
 	printf("\n  └─┴─┴─┴─┴─┴─┴─┘\n");
@@ -51,14 +43,9 @@ void Game::printBoard() {
 
 bool Game::endGame() {
 	long long mask;
-	int lastPlayer = 1 - currentPlayer;
+	byte lastPlayer = !currentPlayer;
 	mask = player[lastPlayer] & (player[lastPlayer] >> 1);
 	if (mask & (mask >> 2)) {
-		return true;
-	}
-	
-	mask = player[lastPlayer] & (player[lastPlayer] << 1);
-	if (mask & (mask << 2)) {
 		return true;
 	}
 	
@@ -67,28 +54,13 @@ bool Game::endGame() {
 		return true;
 	}
 	
-	mask = player[lastPlayer] & (player[lastPlayer] << 7);
-	if (mask & (mask << 14)) {
-		return true;
-	}
-	
 	mask = player[lastPlayer] & (player[lastPlayer] >> 8);
 	if (mask & (mask >> 16)) {
 		return true;
 	}
 	
-	mask = player[lastPlayer] & (player[lastPlayer] << 8);
-	if (mask & (mask << 16)) {
-		return true;
-	}
-	
 	mask = player[lastPlayer] & (player[lastPlayer] >> 6);
 	if (mask & (mask >> 12)) {
-		return true;
-	}
-
-	mask = player[lastPlayer] & (player[lastPlayer] << 6);
-	if (mask & (mask << 12)) {
 		return true;
 	}
 	return false;

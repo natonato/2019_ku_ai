d2798946fcbedda0645364025af37491f26c3d57
natonato <topgun0209@naver.com>
2019-05-02 15:20:05 +0900

    Add files via upload

---
+++ ai.h
@@ -0,0 +1,16 @@
+#ifndef AI_H
+#define AI_H
+
+#include "common.h"
+
+class Ai {
+public:
+	int putStoneAI(Game game);
+	int getScore(Game game);
+	int checkOne(long long a);
+
+
+};
+
+
+#endif //AI_H

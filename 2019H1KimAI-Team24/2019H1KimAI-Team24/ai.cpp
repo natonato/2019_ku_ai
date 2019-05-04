d0cf84f350302e12caa3a61187cf26bac7e9fcaf
이강빈 <rkdqls3079@naver.com>
2019-05-05 01:38:07 +0900

    negmax 수정 및 minmax 추가

--- 2019H1KimAI-Team24/2019H1KimAI-Team24/ai.cpp
+++ 2019H1KimAI-Team24/2019H1KimAI-Team24/ai.cpp
@@ -8,11 +8,11 @@ int Ai::putStoneAI(Game game) {
 	int maxScore = -23, put;
 	
 
-	for (int i=0; i < 7; i++) {
+	for (int i=0; i < 4; i++) {
 		if (game.puttable(order[i])) {		// 현재 state에서 각 열별로 진행한 7개의 state의 점수를 보고 어디로 갈지 결
 			Game currentGame = game.putStone(order[i]);
-			int score = getScore(currentGame, -24, 24);
-
+			int score = -getScore(currentGame, -23, 23);
+			//int score = tmpScore(currentGame, maxScore, 23, false);
 			if (maxScore < score) {
 				maxScore = score;
 				put = order[i];
@@ -28,7 +28,7 @@ int Ai::getScore(Game g, int a, int b) {
 	//Game tmpGame = g;
 	int score=-23, maxScore;
 
-	if (g.state() != g.PLAYING) return 22 - (checkOne(g.board())+1) / 2; //게임 종료, 22 - 승리한 player가 둔 돌 갯수가 score.
+	if (g.state() != g.PLAYING) return -(22 - (g.step + 1) / 2); //게임 종료, 22 - 승리한 player가 둔 돌 갯수가 score.
 	if (g.board() == 0b111111011111101111110111111011111101111110111111) return 0; // 비김
 	if (g.board() == 0b000000000000000000000000000011111101111110111111) return 0;
 
@@ -54,6 +54,44 @@ int Ai::getScore(Game g, int a, int b) {
 	
 
 	return a;
+}
+
+int Ai::tmpScore(Game g, int a, int b, bool max) {
+	int flag = -1;
+	if (!max) {
+		flag = 1;
+	}
+	if (g.state() != g.PLAYING) return flag * (22 - (g.step + 1) / 2);
+	if (g.board() == 0b111111011111101111110111111011111101111110111111) return 0; // 비김
+	if (g.board() == 0b000000000000000000000000000011111101111110111111) return 0;
+	if (max) {
+		int score;
+		for (int i = 0; i < 4; i++) {
+			if (g.puttable(order[i])) {
+				Game tmpGame = g.putStone(order[i]);
+				score = tmpScore(tmpGame, a, b, !max);
+				if (a < score) {
+					a = score;
+				}
+				if (a >= b) break;
+			}
+		}
+		return a;
+	}
+	else {
+		int score;
+		for (int i = 0; i < 4; i++) {
+			if (g.puttable(order[i])) {
+				Game tmpGame = g.putStone(order[i]);
+				score = tmpScore(tmpGame, a, b, !max);
+				if (b > score) {
+					b = score;
+				}
+				if (a >= b) break;
+			}
+		}
+		return b;
+	}
 }
 
 int Ai::checkOne(long long n) {
--- 2019H1KimAI-Team24/2019H1KimAI-Team24/ai.h
+++ 2019H1KimAI-Team24/2019H1KimAI-Team24/ai.h
@@ -8,7 +8,7 @@ public:
 	int putStoneAI(Game game);
 	int getScore(Game game, int a, int b);
 	int checkOne(long long a);
-
+	int tmpScore(Game, int, int, bool);
 	int order[7] = { 3,2,4,1,5,0,6 };
 
 };
